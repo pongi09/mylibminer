f0bdec73bfcb0c7ae2ed915c7995adef4963a4c3
pongi09 <dsdpongi@gmail.com>
2024-06-14 12:09:31 +0200

    -

--- R/func.R
+++ R/func.R
@@ -2,14 +2,22 @@
 #' Provides a brief summary of the package libraries on your machine.
 #' Tamas!
 #'
-#' @return A data.frame containing the count of packages in each othe libraries
+#' @return A data.frame containing the count of packages in each other libraries
 #' @export
 #'
 #' @examples
-lib_summary <- function() {
+#' lib_summary()
+lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    pkg_df$lib_size <- map_dbl(
+      pkg_df$Library,
+      ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+    )
+  }
   pkg_df
 }
